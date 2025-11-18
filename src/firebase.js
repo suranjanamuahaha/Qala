8c09fd17278658de7bdeadc2ccbca2093985fa68
Suranjana Paul <paulsuranjana64@gmail.com>
2025-11-18 21:03:08 +0530

    kms

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATvp_AKxwybNpN2lEmc72Amp1KsZh_25s",
+  authDomain: "qala-27b58.firebaseapp.com",
+  projectId: "qala-27b58",
+  storageBucket: "qala-27b58.firebasestorage.app",
+  messagingSenderId: "566221780079",
+  appId: "1:566221780079:web:ac7ae9645622c77b54c99a"
+};
+
+
+export const app = initializeApp(firebaseConfig);
